5235dfc831ae752b50c7a1644f85b7fdce9eec2f
Dron245 <Dron245@yandex.ru>
2022-12-11 15:03:56 +0300

    1112D

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,14 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+
+document.addEventListener('click', documentActions)
+function documentActions(e){
+	const targetElement = e.target
+	if (targetElement.closest('.milde-header__button')){
+		console.log('t');
+		targetElement.closest('._spoller-init').classList.toggle('_pb')
+	}
+}
+	
